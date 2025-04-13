7bbf0797ce4d04be7cfbdab311c5f62295259aa5
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2025-04-13 19:42:21 +0200

    up

---
+++ Ext.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+namespace WhatsAppBulkSender;
+public class ExitException : Exception { }
+internal static class Ext
+{
+    public static bool Handle(this Fn Fun, IWebDriver driver)
+    {
+        try
+        {
+            return Fun.Invoke(driver);
+        }
+        catch (NoSuchWindowException)
+        {
+            driver.Quit();
+            System.Environment.Exit(0);
+            throw new ExitException();
+        }
+    }
+    public static void WaitForCaptcha(this IWebDriver driver)
+    {
+        try
+        {
+            new WebDriverWait(driver, TimeSpan.FromSeconds(3)).Until(((Fn)(d => d.FindElement(By.TagName("h4")).Displayed)).Handle);
+            for (var retry = 0; retry < 2 && driver.FindElement(By.TagName("h4")).Text is "Enter the characters you see below"; retry++)
+                ((IJavaScriptExecutor)driver).ExecuteScript("window.location.reload()");
+        }
+        catch
+        {
+            Console.WriteLine("Done");
+        }
+    }
+}
